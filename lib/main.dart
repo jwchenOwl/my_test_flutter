b6dd767179c8c7784aca8483f3d1a6a2e84894d8
jwchen <jwchen.page@foxmail.com>
2022-12-22 22:21:22 +0800

    [feat] first

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Welcome to Flutter jwcheb  asdasd ')),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    final _suggestions = <WordPair>[];
+    final _biggerFont = const TextStyle(fontSize: 19);
+    return Text(wordPair.asPascalCase);
+  }
+}
